d5f71df4c97180921d5aba1d411afa91ab8e0090
bhatia_1289 <abhishekbhatia9560@gmail.com>
2026-09-05 23:38:44 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/my_resume')
+def my_resume():
+    return render_template('resume.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
