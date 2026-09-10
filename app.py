9cb263b032423c8648ee6e4941dadcabb1783dd6
bhatia_1289 <abhishekbhatia9560@gmail.com>
2026-09-11 03:29:54 +0530

    Refactor app.py to improve import structure and update main execution block for better environment handling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import os
 app = Flask(__name__)
 
 @app.route('/home')
@@ -22,5 +22,7 @@ def contact_me():
 def skills():
     return render_template('skills.html')
 
-if __name__ == '__main__':
-    app.run(debug = True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
